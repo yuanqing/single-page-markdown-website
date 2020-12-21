43f4baf0b0824f1e4afd2f52dec182f2b4b6a24f
yuanqing <desk@yuanqing.sg>
2020-12-21 09:53:39 +0800

    Use `require.resolve`

--- packages/single-page-markdown-website/src/utilities/build-html-async/read-frontend-lib-file-async.ts
+++ packages/single-page-markdown-website/src/utilities/build-html-async/read-frontend-lib-file-async.ts
@@ -1,4 +1,3 @@
-import * as findUp from 'find-up'
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
@@ -12,16 +11,11 @@ export async function readFrontendLibFileAsync(
 async function resolveFrontendLibFilePathAsync(
   fileName: string
 ): Promise<string> {
-  const filePath = path.join(
-    'node_modules',
-    '@single-page-markdown-website',
-    'frontend',
-    'lib',
-    fileName
+  const filePath = require.resolve(
+    path.join('@single-page-markdown-website', 'frontend', 'lib', fileName)
   )
-  const resolvedFilePath = await findUp(filePath)
-  if (typeof resolvedFilePath === 'undefined') {
-    throw new Error(`Could not resolve file: ${filePath}`)
+  if (typeof filePath === 'undefined') {
+    throw new Error(`Could not resolve file: ${fileName}`)
   }
-  return resolvedFilePath
+  return filePath
 }
